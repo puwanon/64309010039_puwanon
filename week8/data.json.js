6bfd59acf2b244161d1c5cc853031b05557901c0
GodBigg <64309010043@tnk.ac.th>
2022-12-08 09:58:24 +0700

    jsonclass
    workshop8

---
+++ week8/data.json.js
@@ -0,0 +1,11 @@
+let json = '{fname:"sam",lanme:"Smith"}';
+let obj = json.paese(json);
+
+//consol.log(typeof(obj));
+
+let obj2 ={
+    fname : "jack",
+    lname : "Smith"
+};
+let json2 = json.stringify(obj2);
+console.log(json);
